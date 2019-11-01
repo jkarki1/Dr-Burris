a14bf168ee241455591a1d5acf8a3f596901034f
jkarki1 <57273377+jkarki1@users.noreply.github.com>
2019-11-01 17:11:26 -0500

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+package com.example.schoolfrontend;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Student extends AppCompatActivity {
+    /* access modifiers changed from: protected */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView((int) C0262R.layout.activity_student);
+    }
+}
